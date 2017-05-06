cc9881ef1c5f3d700b75d17562aaa0068684345b
Harry Merzin <harry@milosets.me>
2017-05-06 11:55:57 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+export default class App extends React.Component<null, null> {
+
+    render() {
+        return(
+            <div>
+                hello world!
+            </div>
+        )
+    }
+
+}
